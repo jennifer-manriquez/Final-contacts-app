c726f0323266ea4e7e453cb3e78cbcb1617dd734
Jennifer Manriquez <jennymanb@gmail.com>
2021-05-09 01:00:50 -0500

    Templates added

--- app.py
+++ app.py
@@ -1,16 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'FirstName': 'Example1',
+        'LastName': 'Example1Last',
+        'Company': 'Example1Encora',
+        'PhoneNumber': '1111111111', 
+        'Email': 'example1@gmail.com', 
+    },
+    {
+        'FirstName': 'Example2',
+        'LastName': 'Example2Last',
+        'Company': 'Example2Encora',
+        'PhoneNumber': '2222222222', 
+        'Email': 'example2@gmail.com', 
+    }
+]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/newContact")
 def about():
-    return "<h1>New Contact</h1>"
+    return render_template('newContact.html', title='New Contact')
 
 
 if __name__ == '__main__':
