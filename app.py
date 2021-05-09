c56723d3ebc1adf1c051ebdd50eb6712f9b5ffe5
Jennifer Manriquez <jennymanb@gmail.com>
2021-05-09 14:25:40 -0500

    Validation for required and regex

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, url_for, flash, redirect, request
-from forms import newContactForm
 from flask_sqlalchemy import SQLAlchemy
+from wtforms import StringField,  SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, Optional, ValidationError, Regexp
+from flask_wtf import FlaskForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '1ac8277b9ecb407ce9ef4b5932719b0b'
@@ -34,6 +36,33 @@ class Contact(db.Model):
 
     def __repr__(self):
         return f"Contact('{self.FirstName}', '{self.LastName}', '{self.Company}', '{self.PhoneNumber}', '{self.Email}')"
+
+class newContactForm(FlaskForm):
+    only_letters_regex = r"^[a-zA-Z\u00C0-\u017F\s]+$"
+    PhoneNumber_regex = r"^[+]*[\d]{0,4}[\d]{3,4}[0-9]{7,9}$"
+    email_regex = r'^[-\w.]+@([-\w]+\.)+[-\w]{2,4}$'
+
+    FirstName = StringField('First Name',
+                           validators=[DataRequired(),  Length(min=2, max=20), Regexp(only_letters_regex, message="This field must contain alphabetic characters only")])
+    LastName = StringField('Last Name',
+                           validators=[DataRequired(), Length(min=2, max=20), Regexp(only_letters_regex, message="This field must contain alphabetic characters only")])
+    Company = StringField('Company',
+                           validators=[Optional(), Length(min=2, max=20)])   
+    PhoneNumber = StringField('Phone Number',
+                           validators=[Optional(), Length(min=2, max=20), Regexp(PhoneNumber_regex, message="This field must contain a valid phone number input (10 digits)")])                        
+    Email = StringField('Email',
+                        validators=[DataRequired(), Email(), Regexp(email_regex, message="This field must a valid email adress")])
+    submit = SubmitField('Submit contact')
+
+    def validate_PhoneNumber(self, PhoneNumber):
+        contact = Contact.query.filter_by(PhoneNumber=PhoneNumber.data).first()
+        if contact:
+            raise ValidationError('That phone number is taken. Please choose a different one.')
+
+    def validate_Email(self, Email):
+        contact = Contact.query.filter_by(Email=Email.data).first()
+        if contact:
+            raise ValidationError('That email is taken. Please choose a different one.')
 
 
 @app.route("/")
--- forms.py
+++ forms.py
@@ -1,26 +1,31 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,  SubmitField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, Optional, ValidationError
+from wtforms.validators import DataRequired, Length, Email, EqualTo, Optional, ValidationError, Regexp
+from app import Contact
 
 class newContactForm(FlaskForm):
+    only_letters_regex = r"^[a-zA-Z\u00C0-\u017F\s]+$"
+    PhoneNumber_regex = r"^[+]*[\d]{0,4}[\d]{3,4}[0-9]{7,9}$"
+    email_regex = r'^[-\w.]+@([-\w]+\.)+[-\w]{2,4}$'
+
     FirstName = StringField('First Name',
-                           validators=[DataRequired(),  Length(min=2, max=20)])
+                           validators=[DataRequired(),  Length(min=2, max=20), Regexp(only_letters_regex, message="This field must contain alphabetic characters only")])
     LastName = StringField('Last Name',
-                           validators=[DataRequired(), Length(min=2, max=20)])
+                           validators=[DataRequired(), Length(min=2, max=20), Regexp(only_letters_regex, message="This field must contain alphabetic characters only")])
     Company = StringField('Company',
                            validators=[Optional(), Length(min=2, max=20)])   
     PhoneNumber = StringField('Phone Number',
-                           validators=[Optional(), Length(min=2, max=20)])                        
+                           validators=[Optional(), Length(min=2, max=20), Regexp(PhoneNumber_regex, message="This field must contain a valid phone number input (10 digits)")])                        
     Email = StringField('Email',
-                        validators=[DataRequired(), Email()])
+                        validators=[DataRequired(), Email(), Regexp(email_regex, message="This field must a valid email adress")])
     submit = SubmitField('Submit contact')
 
-    #def validate_PhoneNumber(self, phone_number):
-    #    contact = Contact.query.filter_by(PhoneNumber=phone_number.data).first()
-    #    if contact:
-    #        raise ValidationError('That phone number is taken. Please choose a different one.')
+    def validate_PhoneNumber(self, PhoneNumber):
+        contact = Contact.query.filter_by(PhoneNumber=PhoneNumber.data).first()
+        if contact:
+            raise ValidationError('That phone number is taken. Please choose a different one.')
 
-    #def validate_Email(self, Email):
-    #    contact = Contact.query.filter_by(Email=Email.data).first()
-    #    if contact:
-    #        raise ValidationError('That email is taken. Please choose a different one.')
+    def validate_Email(self, Email):
+        contact = Contact.query.filter_by(Email=Email.data).first()
+        if contact:
+            raise ValidationError('That email is taken. Please choose a different one.')
