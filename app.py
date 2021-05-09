1cb3d0b1810149c55f7c0e99361e7ecf42b88205
Jennifer Manriquez <jennymanb@gmail.com>
2021-05-09 00:37:48 -0500

    Added home page and newContact

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask
-
 app = Flask(__name__)
 
+
 @app.route("/")
-def index():
-    return "Hello World!"
+@app.route("/home")
+def home():
+    return "<h1>Home Page</h1>"
+
+
+@app.route("/newContact")
+def about():
+    return "<h1>New Contact</h1>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
